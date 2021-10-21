101dca0fbc205e50a5a81616add322ec40162f38
navctns <navsoft18@gmail.com>
2021-10-21 23:11:07 +0530

    Add existing file

--- ecommerce-product-page-main/app/js/script.js
+++ ecommerce-product-page-main/app/js/script.js
@@ -1,11 +1,24 @@
 
+const toggleBtn = document.querySelector('.header__toggle');
+const headerNav = document.querySelector('#header-nav');
+const headerEl = document.querySelector('.header');
+
+toggleBtn.addEventListener('click', function(){
+    if(headerEl.classList.contains('open')){
+        //close navbar
+        headerEl.classList.remove('open')
+    }else{
+        headerEl.classList.add('open')
+    }
+})
 function toggleNavbar(e){
-    console.log('event', e.target, e.target.classList)
+    // e.preventDefault();
+    console.log('event', e.target,e.target.parentElement, e.target.classList)
     //add open class
-    if(e.target.classList.contains("open")){//close navbar
-        e.target.classList.remove("open")
+    if(e.target.parentElement.classList.contains("open")){//close navbar
+        e.target.parentElement.classList.remove("open")
     }else{//open navbar
-        e.target.classList.add("open");
+        e.target.parentElement.classList.add("open");
     }
     
 }
